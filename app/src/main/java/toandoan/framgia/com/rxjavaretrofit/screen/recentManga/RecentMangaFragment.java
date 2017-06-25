b9d05003a1dc8062e4d5650fd216230ada7fe8ae
DoanVanToan <toandoan.coder@gmail.com>
2017-06-25 09:48:57 +0700

    add recent ui

--- app/src/main/java/toandoan/framgia/com/rxjavaretrofit/screen/recentManga/RecentMangaFragment.java
+++ app/src/main/java/toandoan/framgia/com/rxjavaretrofit/screen/recentManga/RecentMangaFragment.java
@@ -3,13 +3,15 @@ package toandoan.framgia.com.rxjavaretrofit.screen.recentManga;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import toandoan.framgia.com.rxjavaretrofit.R;
 import toandoan.framgia.com.rxjavaretrofit.databinding.FragmentRecentMangaBinding;
 import toandoan.framgia.com.rxjavaretrofit.screen.BaseFragment;
+import toandoan.framgia.com.rxjavaretrofit.utils.widget.itemtouch.ItemTouchHelperExtension;
 
 /**
  * RecentManga Screen.
@@ -17,6 +19,10 @@ import toandoan.framgia.com.rxjavaretrofit.screen.BaseFragment;
 public class RecentMangaFragment extends BaseFragment {
 
     private RecentMangaContract.ViewModel mViewModel;
+    private RecyclerView mRecyclerView;
+    private RecentMangaAdapter mAdapter;
+    public ItemTouchHelperExtension mItemTouchHelper;
+    public ItemTouchHelperExtension.Callback mCallback;
 
     public static RecentMangaFragment newInstance() {
         return new RecentMangaFragment();
@@ -39,6 +45,16 @@ public class RecentMangaFragment extends BaseFragment {
         FragmentRecentMangaBinding binding =
                 DataBindingUtil.inflate(inflater, R.layout.fragment_recent_manga, container, false);
         binding.setViewModel((RecentMangaViewModel) mViewModel);
+
+        mRecyclerView = binding.recyclerRecent;
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAdapter = new RecentMangaAdapter(getContext());
+        mRecyclerView.setAdapter(mAdapter);
+        mCallback = new ItemTouchHelperCallback();
+        mItemTouchHelper = new ItemTouchHelperExtension(mCallback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+        mAdapter.setItemTouchHelperExtension(mItemTouchHelper);
         return binding.getRoot();
     }
 
---
+++ app/src/main/java/toandoan/framgia/com/rxjavaretrofit/utils/widget/itemtouch/Extension.java
@@ -0,0 +1,6 @@
+package toandoan.framgia.com.rxjavaretrofit.utils.widget.itemtouch;
+
+public interface Extension {
+
+    float getActionWidth();
+}
